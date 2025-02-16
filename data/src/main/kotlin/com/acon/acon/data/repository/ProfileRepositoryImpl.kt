5d066544f519f0e7cb8b62e57b5634821d6c7359
seognmin <arcn1345@gmail.com>
2025-02-16 21:08:48 +0900

    Fix: ProfileRepositoryImpl에서 ProfileRepository 구현 추가

--- data/src/main/kotlin/com/acon/acon/data/repository/ProfileRepositoryImpl.kt
+++ data/src/main/kotlin/com/acon/acon/data/repository/ProfileRepositoryImpl.kt
@@ -3,12 +3,13 @@ package com.acon.acon.data.repository
 import com.acon.acon.data.datasource.remote.ProfileRemoteDataSource
 import com.acon.acon.data.error.runCatchingWith
 import com.acon.acon.domain.model.profile.Profile
+import com.acon.acon.domain.repository.ProfileRepository
 import javax.inject.Inject
 
 class ProfileRepositoryImpl @Inject constructor(
     private val profileRemoteDataSource: ProfileRemoteDataSource
-) {
-    suspend fun fetchProfile(): Result<Profile> {
+) : ProfileRepository {
+    override suspend fun fetchProfile(): Result<Profile> {
         return runCatchingWith() {
             profileRemoteDataSource.fetchProfile().toProfile()
         }
