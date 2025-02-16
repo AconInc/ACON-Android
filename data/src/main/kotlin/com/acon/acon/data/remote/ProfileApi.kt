efb02a38c1c43add82a39aa6253075efc489d141
seognmin <arcn1345@gmail.com>
2025-02-17 02:26:58 +0900

    Fix: ProfileResponse의 birthDate nullable로 수정

--- data/src/main/kotlin/com/acon/acon/data/remote/ProfileApi.kt
+++ data/src/main/kotlin/com/acon/acon/data/remote/ProfileApi.kt
@@ -4,7 +4,6 @@ import com.acon.acon.data.dto.response.profile.ProfileResponse
 import retrofit2.http.GET
 
 interface ProfileApi {
-
     @GET("/api/v1/members/me")
     suspend fun fetchProfile(): ProfileResponse
 }
