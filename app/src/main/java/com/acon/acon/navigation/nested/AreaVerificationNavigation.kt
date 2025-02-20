cb6981202fb3616a2f4b01947ea1252e5b489841
seognmin <arcn1345@gmail.com>
2025-02-21 01:32:26 +0900

    Feat: AreaVerificationRoute 경로 전달 및 경로 별 분기 처리 (온보딩 분기)

--- app/src/main/java/com/acon/acon/navigation/nested/AreaVerificationNavigation.kt
+++ app/src/main/java/com/acon/acon/navigation/nested/AreaVerificationNavigation.kt
@@ -7,6 +7,7 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.navigation
 import androidx.navigation.toRoute
+import com.acon.acon.feature.SettingsRoute
 import com.acon.acon.feature.areaverification.AreaVerificationRoute
 import com.acon.acon.feature.areaverification.AreaVerificationScreenContainer
 import com.acon.acon.feature.areaverification.PreferenceMapScreen
@@ -16,23 +17,31 @@ fun NavGraphBuilder.areaVerificationNavigation(
     navController: NavHostController
 ) {
     navigation<AreaVerificationRoute.Graph>(
-        startDestination = AreaVerificationRoute.RequireAreaVerification
+        startDestination = AreaVerificationRoute.RequireAreaVerification()
     ) {
-        composable<AreaVerificationRoute.RequireAreaVerification> {
+        composable<AreaVerificationRoute.RequireAreaVerification> { backStackEntry ->
+
+            val routeData = backStackEntry.arguments?.let {
+                AreaVerificationRoute.RequireAreaVerification(
+                    route = it.getString("route")
+                )
+            }
+
             AreaVerificationScreenContainer(
                 modifier = Modifier.fillMaxSize(),
                 onNewAreaClick = { latitude, longitude ->
                     navController.navigate(
-                        AreaVerificationRoute.CheckInMap(latitude, longitude)
+                        AreaVerificationRoute.CheckInMap(latitude, longitude, routeData?.route)
                     )
                 },
                 onNextScreen = { latitude, longitude ->
                     navController.navigate(
-                        AreaVerificationRoute.CheckInMap(latitude, longitude)
+                        AreaVerificationRoute.CheckInMap(latitude, longitude, routeData?.route)
                     )
                 }
             )
         }
+
         composable<AreaVerificationRoute.CheckInMap> { backStackEntry ->
             val route = backStackEntry.toRoute<AreaVerificationRoute.CheckInMap>()
 
@@ -43,7 +52,15 @@ fun NavGraphBuilder.areaVerificationNavigation(
                     navController.navigate(AreaVerificationRoute.Complete)
                 },
                 onNavigateToNext = {
-                    navController.navigate(OnboardingRoute.Graph)
+                    if(route.route == "settings") {
+                        navController.navigate(SettingsRoute.LocalVerification) {
+                            popUpTo(AreaVerificationRoute.Graph) {
+                                inclusive = true
+                            }
+                        }
+                    } else {
+                        navController.navigate(OnboardingRoute.Graph)
+                    }
                 },
                 onBackClick = {
                     navController.popBackStack()
