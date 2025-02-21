987fb2b0f3084415be7b0fa36cdecf6ce3e18b5b
seognmin <arcn1345@gmail.com>
2025-02-22 01:15:22 +0900

    Del: 파일 삭제

--- domain/src/main/java/com/acon/acon/domain/model/area/SettingsVerifiedArea.kt
+++
@@ -1,9 +0,0 @@
-package com.acon.acon.domain.model.area
-
-import androidx.compose.runtime.Immutable
-
-@Immutable
-data class SettingsVerifiedArea(
-    val verifiedAreaId: Long,
-    val name: String
-)
