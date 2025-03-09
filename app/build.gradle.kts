e591da65f26a7560b42d34df434406f2b8ae7bac
seognmin <arcn1345@gmail.com>
2025-03-10 01:34:34 +0900

    mod: versionCode 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         applicationId = "com.acon.acon"
         minSdk = 28
         targetSdk = 35
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
