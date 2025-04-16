93580d5071c4f637723d6c8400118deb78e4837f
Thirfir <sangbbang31@gmail.com>
2025-04-16 19:26:34 +0900

    feat: 리포지토리 구조 개선
    - UserType 추가
    
    - Social 및 User 리포지토리 개선

--- data/src/main/kotlin/com/acon/acon/data/repository/SocialRepositoryImpl.kt
+++ data/src/main/kotlin/com/acon/acon/data/repository/SocialRepositoryImpl.kt
@@ -1,25 +1,25 @@
 package com.acon.acon.data.repository
 
-import com.acon.acon.data.datasource.local.TokenLocalDataSource
 import com.acon.acon.data.datasource.remote.TokenRemoteDataSource
 import com.acon.acon.data.error.runCatchingWith
+import com.acon.acon.domain.error.user.PostLoginError
 import com.acon.acon.domain.model.user.VerificationStatus
-import com.acon.acon.domain.repository.UserRepository
 import com.acon.acon.domain.repository.SocialRepository
+import com.acon.acon.domain.repository.UserRepository
 import com.acon.acon.domain.type.SocialType
 import javax.inject.Inject
 
 class SocialRepositoryImpl @Inject constructor(
     private val tokenRemoteDataSource: TokenRemoteDataSource,
     private val userRepository: UserRepository,
-    private val tokenLocalDataSource: TokenLocalDataSource
 ) : SocialRepository {
-    override suspend fun signIn(): Result<VerificationStatus> {
-        return runCatchingWith() {
-            val idToken = tokenRemoteDataSource.signIn().getOrThrow()
-            tokenLocalDataSource.saveGoogleIdToken(idToken)
+    override suspend fun googleLogin(): Result<VerificationStatus> {
+        return runCatchingWith(*PostLoginError.createErrorInstances()) {
+            println("ddddddddddd11")
+            val idToken = tokenRemoteDataSource.googleLogin().getOrThrow()
+            println("ddddddddddd22")
 
-            userRepository.postLogin(
+            userRepository.login(
                 socialType = SocialType.GOOGLE, idToken = idToken
             ).getOrThrow()
         }
