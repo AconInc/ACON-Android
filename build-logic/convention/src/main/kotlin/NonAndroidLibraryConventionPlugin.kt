61d362e5bbe5559a00de6b2d7e0f2659aacfded4
Thirfir <sangbbang31@gmail.com>
2025-04-21 23:54:05 +0900

    feat: 자바 or 코틀린 라이브러리 플러그인

---
+++ build-logic/convention/src/main/kotlin/NonAndroidLibraryConventionPlugin.kt
@@ -0,0 +1,32 @@
+import org.gradle.api.JavaVersion
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.plugins.JavaPluginExtension
+import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.dependencies
+import utils.catalog
+import utils.compileOnly
+import utils.implementation
+
+class NonAndroidLibraryConventionPlugin: Plugin<Project> {
+
+    override fun apply(target: Project) {
+        target.run {
+            pluginManager.run {
+                apply("java-library")
+                apply("org.jetbrains.kotlin.jvm")
+            }
+
+            extensions.configure<JavaPluginExtension> {
+                sourceCompatibility = JavaVersion.VERSION_17
+                targetCompatibility = JavaVersion.VERSION_17
+            }
+
+            dependencies {
+                implementation(catalog.findLibrary("javax-inject").get())
+                implementation(catalog.findLibrary("kotlinx-coroutines-core").get())
+                compileOnly(catalog.findLibrary("compose.stable.marker").get())
+            }
+        }
+    }
+}
