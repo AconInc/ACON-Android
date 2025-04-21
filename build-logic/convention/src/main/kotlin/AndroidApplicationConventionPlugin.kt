e9bbb959c73cfa314f4d545b08c843b03cc09951
Thirfir <sangbbang31@gmail.com>
2025-04-21 23:51:39 +0900

    feat: Android Application 플러그인

---
+++ build-logic/convention/src/main/kotlin/AndroidApplicationConventionPlugin.kt
@@ -0,0 +1,34 @@
+import com.android.build.api.dsl.ApplicationExtension
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
+import utils.catalog
+import utils.configureKotlinAndroid
+
+class AndroidApplicationConventionPlugin: Plugin<Project> {
+
+    override fun apply(target: Project) {
+        target.run {
+            pluginManager.run {
+                apply("com.android.application")
+                apply("org.jetbrains.kotlin.android")
+                apply("com.google.devtools.ksp")
+            }
+
+            extensions.configure<ApplicationExtension> {
+                packaging {
+                    resources {
+                        excludes += "/META-INF/{AL2.0,LGPL2.1}"
+                    }
+                }
+                defaultConfig {
+                    applicationId = catalog.findVersion("projectApplicationId").get().toString()
+                    targetSdk = catalog.findVersion("projectTargetSdk").get().toString().toInt()
+                    versionCode = catalog.findVersion("projectVersionCode").get().toString().toInt()
+                    versionName = catalog.findVersion("projectVersionName").get().toString()
+                }
+                configureKotlinAndroid(this)
+            }
+        }
+    }
+}
