4bbb364bbee122766d53dee32a83958221bce621
Thirfir <sangbbang31@gmail.com>
2025-04-21 23:53:43 +0900

    feat: Android Library 플러그인

---
+++ build-logic/convention/src/main/kotlin/AndroidLibraryConventionPlugin.kt
@@ -0,0 +1,23 @@
+import com.android.build.api.dsl.LibraryExtension
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
+import utils.configureKotlinAndroid
+
+class AndroidLibraryConventionPlugin: Plugin<Project> {
+
+    override fun apply(target: Project) {
+        target.run {
+            pluginManager.run {
+                apply("com.android.library")
+                apply("org.jetbrains.kotlin.android")
+                apply("com.google.devtools.ksp")
+                apply("kotlin-parcelize")
+            }
+
+            extensions.configure<LibraryExtension> {
+                configureKotlinAndroid(this)
+            }
+        }
+    }
+}
