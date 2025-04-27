cb097f4a362eef3b6761977884b825bac55071cf
seongmin.kim <arcn1345@gmail.com>
2025-04-27 16:59:22 +0900

    mod: 토스트 메시지 표시 시간 Long -> Short로 수정

--- core/utils/feature/src/main/java/com/acon/acon/core/utils/feature/toast/showToast.kt
+++ core/utils/feature/src/main/java/com/acon/acon/core/utils/feature/toast/showToast.kt
@@ -4,5 +4,5 @@ import android.content.Context
 import android.widget.Toast
 
 fun Context.showToast(message: Int) {
-    Toast.makeText(this, getString(message), Toast.LENGTH_LONG).show()
+    Toast.makeText(this, getString(message), Toast.LENGTH_SHORT).show()
 }
