e71e1b4fa480ed6e83f6f195bc779a8f2699fc9a
Thirfir <sangbbang31@gmail.com>
2025-04-27 22:09:53 +0900

    feat: TextField 2.0 컴포넌트

---
+++ core/designsystem/src/main/java/com/acon/acon/core/designsystem/component/textfield/AconDefaultTextField.kt
@@ -0,0 +1,81 @@
+package com.acon.acon.core.designsystem.component.textfield
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.text.TextLayoutResult
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.acon.acon.core.designsystem.blur.LocalHazeState
+import com.acon.acon.core.designsystem.blur.defaultHazeEffect
+import com.acon.acon.core.designsystem.theme.AconTheme
+
+@Composable
+fun AconDefaultTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    readOnly: Boolean = false,
+    textStyle: TextStyle = AconTheme.typography.Title4.copy(fontWeight = FontWeight.Normal, color = AconTheme.color.White),
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    keyboardActions: KeyboardActions = KeyboardActions.Default,
+    maxLines: Int = Int.MAX_VALUE,
+    minLines: Int = 1,
+    visualTransformation: VisualTransformation = VisualTransformation.None,
+    onTextLayout: (TextLayoutResult) -> Unit = {},
+    decorationBox: @Composable (innerTextField: @Composable () -> Unit) -> Unit = @Composable { innerTextField -> innerTextField() }
+) {
+    BasicTextField(
+        value = value,
+        onValueChange = onValueChange,
+        modifier = modifier
+            .background(
+                shape = RoundedCornerShape(10.dp),
+                color = AconTheme.color.GlassWhiteDefault
+            ).padding(horizontal = 8.dp, vertical = 10.dp)
+            .defaultHazeEffect(
+                hazeState = LocalHazeState.current,
+                tintColor = AconTheme.color.GlassWhiteDefault,
+            ),
+        enabled = enabled,
+        readOnly = readOnly,
+        textStyle = textStyle,
+        keyboardOptions = keyboardOptions,
+        keyboardActions = keyboardActions,
+        singleLine = false,
+        maxLines = maxLines,
+        minLines = minLines,
+        visualTransformation = visualTransformation,
+        onTextLayout = onTextLayout,
+        cursorBrush = SolidColor(AconTheme.color.Action)
+    ) { innerTextField ->
+        decorationBox(innerTextField)
+    }
+}
+
+@Composable
+@Preview
+private fun AconDefaultTextFieldPreview(
+) {
+    AconDefaultTextField(
+        value = "Search Text",
+        onValueChange = { },
+        modifier = Modifier.fillMaxWidth(),
+    ) { innerTextField ->
+        innerTextField()
+    }
+}
