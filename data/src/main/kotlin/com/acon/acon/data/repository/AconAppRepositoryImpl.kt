2daebe4a7caa16dfe670063149405ce7723b02a6
Thirfir <sangbbang31@gmail.com>
2025-04-29 02:06:12 +0900

    feat: 업데이트 Local Source - Repository연동

--- data/src/main/kotlin/com/acon/acon/data/repository/AconAppRepositoryImpl.kt
+++ data/src/main/kotlin/com/acon/acon/data/repository/AconAppRepositoryImpl.kt
@@ -1,16 +1,30 @@
 package com.acon.acon.data.repository
 
+import com.acon.acon.data.datasource.local.AconAppLocalDataSource
 import com.acon.acon.data.datasource.remote.AconAppRemoteDataSource
 import com.acon.acon.data.error.runCatchingWith
 import com.acon.acon.domain.repository.AconAppRepository
 import javax.inject.Inject
 
 class AconAppRepositoryImpl @Inject constructor(
-    private val aconAppRemoteDataSource: AconAppRemoteDataSource
+    private val aconAppRemoteDataSource: AconAppRemoteDataSource,
+    private val aconAppLocalDataSource: AconAppLocalDataSource
 ) : AconAppRepository {
     override suspend fun fetchShouldUpdateApp(): Result<Boolean> {
         return runCatchingWith {
             aconAppRemoteDataSource.fetchShouldUpdateApp()
+        }
+    }
+
+    override suspend fun setUpdatePostponeTime(time: Long): Result<Unit> {
+        return runCatchingWith {
+            aconAppLocalDataSource.setUpdatePostponeTime(time)
+        }
+    }
+
+    override suspend fun getUpdatePostponeTime(): Result<Long> {
+        return runCatchingWith {
+            aconAppLocalDataSource.getUpdatePostponeTime()
         }
     }
 }
--- domain/src/main/java/com/acon/acon/domain/repository/AconAppRepository.kt
+++ domain/src/main/java/com/acon/acon/domain/repository/AconAppRepository.kt
@@ -2,4 +2,6 @@ package com.acon.acon.domain.repository
 
 interface AconAppRepository {
     suspend fun fetchShouldUpdateApp(): Result<Boolean>
+    suspend fun setUpdatePostponeTime(time: Long): Result<Unit>
+    suspend fun getUpdatePostponeTime(): Result<Long>
 }
