9ed4fb90491355440781ba17c01eb191970d994b
Thirfir <sangbbang31@gmail.com>
2025-05-07 02:51:14 +0900

    feat: 바텀시트 컴포넌트

---
+++ core/designsystem/src/main/java/com/acon/acon/core/designsystem/component/bottomsheet/AconBottomSheet.kt
@@ -0,0 +1,114 @@
+package com.acon.acon.core.designsystem.component.bottomsheet
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.BottomSheetDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.ModalBottomSheetDefaults
+import androidx.compose.material3.ModalBottomSheetProperties
+import androidx.compose.material3.SheetState
+import androidx.compose.material3.SheetValue
+import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.acon.acon.core.designsystem.glassmorphism.LocalHazeState
+import com.acon.acon.core.designsystem.glassmorphism.defaultHazeEffect
+import com.acon.acon.core.designsystem.theme.AconTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AconBottomSheet(
+    onDismissRequest: () -> Unit,
+    modifier: Modifier = Modifier,
+    sheetState: SheetState = rememberModalBottomSheetState(
+        skipPartiallyExpanded = true
+    ),
+    sheetMaxWidth: Dp = BottomSheetDefaults.SheetMaxWidth,
+    shape: Shape = RoundedCornerShape(topStart = 20.dp, topEnd = 20.dp),
+    containerColor: Color = AconTheme.color.Gray800,
+    contentColor: Color = AconTheme.color.White,
+    tonalElevation: Dp = 0.dp,
+    scrimColor: Color = BottomSheetDefaults.ScrimColor,
+    dragHandle: @Composable (() -> Unit) = {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 10.dp),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Box(
+                modifier = Modifier
+                    .width(48.dp)
+                    .height(6.dp)
+                    .background(
+                        color = AconTheme.color.Gray500,
+                        shape = CircleShape
+                    )
+            )
+        }
+    },
+    contentWindowInsets: @Composable () -> WindowInsets = { BottomSheetDefaults.windowInsets },
+    properties: ModalBottomSheetProperties = ModalBottomSheetDefaults.properties,
+    content: @Composable (ColumnScope.() -> Unit)
+) {
+    ModalBottomSheet(
+        onDismissRequest = onDismissRequest,
+        sheetState = sheetState,
+        modifier = modifier,
+        sheetMaxWidth = sheetMaxWidth,
+        shape = shape,
+        contentColor = contentColor,
+        tonalElevation = tonalElevation,
+        scrimColor = scrimColor,
+        dragHandle = null,
+        contentWindowInsets = contentWindowInsets,
+        properties = properties,
+    ) {
+        Column(
+            modifier = Modifier
+                .defaultHazeEffect(
+                    hazeState = LocalHazeState.current,
+                    tintColor = containerColor,
+                    alpha = .8f
+                )
+        ) {
+            dragHandle()
+            content()
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+@Preview
+private fun AconBottomSheetPreview() {
+    AconBottomSheet(
+        modifier = Modifier.fillMaxSize(),
+        onDismissRequest = {},
+        content = {},
+        sheetState = SheetState(
+            density = LocalDensity.current,
+            initialValue = SheetValue.PartiallyExpanded,
+            skipPartiallyExpanded = false,
+        )
+    )
+}
