21175c6a1b068543bf84305c109cf07072b98fa6
Thirfir <sangbbang31@gmail.com>
2025-05-13 01:23:23 +0900

    chore: SocialRepository EntryPoint 추가

--- feature/common/src/main/java/com/acon/feature/common/di/ComposableEntryPoints.kt
+++ feature/common/src/main/java/com/acon/feature/common/di/ComposableEntryPoints.kt
@@ -1,12 +1,20 @@
 package com.acon.feature.common.di
 
+import com.acon.acon.domain.repository.SocialRepository
 import com.acon.acon.domain.repository.UserRepository
 import dagger.hilt.EntryPoint
 import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
 import dagger.hilt.components.SingletonComponent
 
 @EntryPoint
 @InstallIn(SingletonComponent::class)
 interface ComposableEntryPoint {
     fun userRepository(): UserRepository
+}
+
+@EntryPoint
+@InstallIn(ActivityComponent::class)
+interface ComposableActivityEntryPoint {
+    fun socialRepository(): SocialRepository
 }
