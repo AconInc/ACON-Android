c3780513930ca6cc57eb2d2bb8e5bde74accce9d
Thirfir <sangbbang31@gmail.com>
2025-05-13 01:45:01 +0900

    feat: enableEdgeToEdge 추가

--- app/src/main/java/com/acon/acon/MainActivity.kt
+++ app/src/main/java/com/acon/acon/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
@@ -34,6 +35,7 @@ class MainActivity : ComponentActivity() {
             Timber.plant(Timber.DebugTree())
         }
 
+        enableEdgeToEdge()
         setContent {
             AconTheme {
                 AconNavigation(
