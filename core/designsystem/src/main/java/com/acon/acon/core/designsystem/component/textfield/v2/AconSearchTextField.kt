5aba989c35b236bc228e49acfba9c2a06ae3b359
Thirfir <sangbbang31@gmail.com>
2025-05-14 00:07:27 +0900

    fix: search 텍스트필드 높이 조정

--- core/designsystem/src/main/java/com/acon/acon/core/designsystem/component/textfield/v2/AconSearchTextField.kt
+++ core/designsystem/src/main/java/com/acon/acon/core/designsystem/component/textfield/v2/AconSearchTextField.kt
@@ -1,24 +1,31 @@
 package com.acon.acon.core.designsystem.component.textfield.v2
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.TextLayoutResult
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.rememberTextMeasurer
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import com.acon.acon.core.designsystem.R
 import com.acon.acon.core.designsystem.noRippleClickable
@@ -38,6 +45,15 @@ fun AconSearchTextField(
     visualTransformation: VisualTransformation = VisualTransformation.None,
     onTextLayout: (TextLayoutResult) -> Unit = {},
 ) {
+    val textMeasurer = rememberTextMeasurer()
+    val density = LocalDensity.current
+    val result = textMeasurer.measure(text = "▪", style = textStyle)
+
+    val height = remember {
+        with(density) {
+            result.size.height.toDp()
+        }
+    }
     AconFilledTextField(
         value = value,
         onValueChange = onValueChange,
@@ -63,13 +79,18 @@ fun AconSearchTextField(
                     modifier = Modifier.width(18.dp)
                 )
                 Spacer(modifier = Modifier.width(8.dp))
-                if (value.isEmpty()) {
-                    Text(
-                        text = placeholder,
-                        style = textStyle.copy(color = AconTheme.color.Gray500),
-                    )
+                Box(
+                    modifier = Modifier.height(height),
+                    contentAlignment = Alignment.CenterStart
+                ) {
+                    if (value.isEmpty()) {
+                        Text(
+                            text = placeholder,
+                            style = textStyle.copy(color = AconTheme.color.Gray500),
+                        )
+                    }
+                    innerTextField()
                 }
-                innerTextField()
             }
             if (value.isNotEmpty()) {
                 Spacer(modifier = Modifier.width(8.dp))
