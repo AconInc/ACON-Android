b573d5d8dd727a345159640dae6c50164082073e
Thirfir <sangbbang31@gmail.com>
2025-05-14 00:02:58 +0900

    feat: 업로드 뷰모델 추가

---
+++ feature/upload/src/main/java/com/acon/acon/feature/upload/v2/UploadSearchViewModel.kt
@@ -0,0 +1,52 @@
+package com.acon.acon.feature.upload.v2
+
+import androidx.compose.runtime.Immutable
+import com.acon.acon.core.utils.feature.base.BaseContainerHost
+import com.acon.acon.domain.model.upload.v2.SearchedSpot
+import dagger.hilt.android.lifecycle.HiltViewModel
+import org.orbitmvi.orbit.annotation.OrbitExperimental
+import org.orbitmvi.orbit.viewmodel.container
+import javax.inject.Inject
+
+@OptIn(OrbitExperimental::class)
+@HiltViewModel
+class UploadSearchViewModel @Inject constructor(
+
+) : BaseContainerHost<UploadSearchUiState, UploadSearchSideEffect>() {
+    override val container = container<UploadSearchUiState, UploadSearchSideEffect>(UploadSearchUiState.Success()) {
+        // Init state
+    }
+
+    fun onSearchQueryChanged(query: String) = intent {
+        runOn<UploadSearchUiState.Success> {
+            reduce {
+                state.copy(
+                    query = query
+                )
+            }
+        }
+    }
+
+    fun onBackAction() = intent {
+        postSideEffect(UploadSearchSideEffect.NavigateBack)
+    }
+
+    fun onNextAction() = intent {
+        postSideEffect(UploadSearchSideEffect.NavigateToReviewScreen)
+    }
+}
+
+sealed interface UploadSearchUiState {
+    @Immutable
+    data class Success(
+        val recommends: List<String> = listOf(),
+        val query: String = "",
+        val searchedSpots: List<SearchedSpot> = listOf(),
+    ) : UploadSearchUiState
+    data object LoadFailed : UploadSearchUiState
+}
+
+sealed interface UploadSearchSideEffect {
+    data object NavigateToReviewScreen : UploadSearchSideEffect
+    data object NavigateBack : UploadSearchSideEffect
+}
