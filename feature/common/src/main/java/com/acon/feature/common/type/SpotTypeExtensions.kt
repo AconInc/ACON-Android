4b78bed9ca5f7cefedd725c029be7d9bb534d1eb
Thirfir <sangbbang31@gmail.com>
2025-05-14 14:34:58 +0900

    fix: 장소 타입 한글 변환 버그 수정

--- feature/common/src/main/java/com/acon/feature/common/type/SpotTypeExtensions.kt
+++ feature/common/src/main/java/com/acon/feature/common/type/SpotTypeExtensions.kt
@@ -5,7 +5,7 @@ import com.acon.acon.core.designsystem.R
 
 fun SpotType.getNameResId(): Int {
     return when (this) {
-        SpotType.RESTAURANT -> R.string.cafe
-        SpotType.CAFE -> R.string.restaurant
+        SpotType.RESTAURANT -> R.string.restaurant
+        SpotType.CAFE -> R.string.cafe
     }
 }
