72f63ca3da73f26bee92d55d9f22ef695f49c747
Thirfir <sangbbang31@gmail.com>
2025-05-14 16:06:19 +0900

    chore: 도메인에 Serialization 의존성 추가

--- domain/src/main/java/com/acon/acon/domain/model/upload/v2/SearchedSpot.kt
+++ domain/src/main/java/com/acon/acon/domain/model/upload/v2/SearchedSpot.kt
@@ -2,8 +2,10 @@ package com.acon.acon.domain.model.upload.v2
 
 import androidx.compose.runtime.Immutable
 import com.acon.acon.domain.type.SpotType
+import kotlinx.serialization.Serializable
 
 @Immutable
+@Serializable
 data class SearchedSpot(
     val spotId: Long,
     val name: String,
