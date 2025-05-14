c3dd3f8d14d8a141812945a3ca744268dbde5295
Thirfir <sangbbang31@gmail.com>
2025-05-14 14:35:18 +0900

    chore: UploadUiState 테스트 객체

--- feature/upload/src/main/java/com/acon/acon/feature/upload/mock/UploadMock.kt
+++ feature/upload/src/main/java/com/acon/acon/feature/upload/mock/UploadMock.kt
@@ -1,5 +1,7 @@
 package com.acon.acon.feature.upload.mock
 
+import com.acon.acon.domain.model.upload.v2.SearchedSpot
+import com.acon.acon.domain.type.SpotType
 import com.acon.acon.feature.upload.v2.UploadSearchUiState
 
 internal val uploadSearchUiStateMock = UploadSearchUiState.Success(
@@ -11,5 +13,48 @@ internal val uploadSearchUiStateMock = UploadSearchUiState.Success(
         "맥도날드",
     ),
     query = "",
-    searchedSpots = listOf()
+    searchedSpots = listOf(
+        SearchedSpot(
+            spotId = 1L,
+            name = "BHC 치킨",
+            address = "서울특별시 강남구 삼성동 123-45",
+            spotType = SpotType.RESTAURANT,
+        ),
+        SearchedSpot(
+            spotId = 2L,
+            name = "BBQ",
+            address = "서울특별시 강남구 삼성동 123-4567",
+            spotType = SpotType.RESTAURANT,
+        ),
+        SearchedSpot(
+            spotId = 3L,
+            name = "아라치",
+            address = "서울특별시 강남구 삼성동 123-45",
+            spotType = SpotType.RESTAURANT,
+        ),
+        SearchedSpot(
+            spotId = 4L,
+            name = "교촌치킨",
+            address = "서울특별시 강남구 삼성동 123-45",
+            spotType = SpotType.RESTAURANT,
+        ),
+        SearchedSpot(
+            spotId = 5L,
+            name = "처갓집",
+            address = "서울특별시 강남구 삼성동 123-45",
+            spotType = SpotType.RESTAURANT,
+        ),
+        SearchedSpot(
+            spotId = 6L,
+            name = "굽네치킨",
+            address = "서울특별시 강남구 삼성동 123-45",
+            spotType = SpotType.RESTAURANT,
+        ),
+        SearchedSpot(
+            spotId = 7L,
+            name = "네네치킨",
+            address = "서울특별시 강남구 삼성동 123-45",
+            spotType = SpotType.RESTAURANT,
+        ),
+    )
 )
