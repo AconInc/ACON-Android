8d3f4a26d4f00881a95117f97a5fa5f543532107
Thirfir <sangbbang31@gmail.com>
2025-05-16 20:20:25 +0900

    feat: 온보딩 내비게이션 설정

---
+++ feature/onboarding/src/main/java/com/acon/acon/feature/onboarding/screen/composable/ChooseDislikesScreenContainer.kt
@@ -0,0 +1,12 @@
+package com.acon.acon.feature.onboarding.screen.composable
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ChooseDislikesScreenContainer(
+    onComplete: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+
+}
