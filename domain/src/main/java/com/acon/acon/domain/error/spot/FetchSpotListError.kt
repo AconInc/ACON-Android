75d01a42855bbccfac9028381e9d89eb808616a7
Thirfir <sangbbang31@gmail.com>
2025-05-19 19:22:02 +0900

    chore: 에러 코드 수정

--- domain/src/main/java/com/acon/acon/domain/error/spot/FetchSpotListError.kt
+++ domain/src/main/java/com/acon/acon/domain/error/spot/FetchSpotListError.kt
@@ -6,21 +6,20 @@ import com.acon.acon.domain.error.RootError
 sealed class FetchSpotListError : RootError() {
 
     class InvalidSpotType : FetchSpotListError() {
-        override val code: Int = 40018
+        override val code: Int = 40015
     }
     class InvalidCategory : FetchSpotListError() {
-        override val code: Int = 40019
+        override val code: Int = 40018
     }
     class InvalidOption : FetchSpotListError() {
-        override val code: Int = 40020
+        override val code: Int = 40019
     }
     class NonMatchingSpotTypeAndCategory : FetchSpotListError() {
-        override val code: Int = 40021
+        override val code: Int = 40020
     }
     class NonMatchingCategoryAndOption : FetchSpotListError() {
-        override val code: Int = 40022
+        override val code: Int = 40021
     }
-
     class OutOfServiceAreaError : FetchSpotListError() {
         override val code: Int = 40405
     }
