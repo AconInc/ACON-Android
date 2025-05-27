9f399e3b147b78fea7473835966f90ea078bda96
seongmin.kim <arcn1345@gmail.com>
2025-05-28 01:57:49 +0900

    feat: AreaVerificationHomeScreen Ui 구현 및 추가

--- feature/areaverification/src/main/java/com/acon/acon/feature/areaverification/AreaVerificationHomeScreen.kt
+++ feature/areaverification/src/main/java/com/acon/acon/feature/areaverification/AreaVerificationHomeScreen.kt
@@ -1,72 +1,69 @@
 package com.acon.acon.feature.areaverification
 
-import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.draw.paint
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import com.acon.acon.core.designsystem.R
 import com.acon.acon.core.designsystem.component.button.v2.AconFilledButton
-import com.acon.acon.core.designsystem.innerShadow
+import com.acon.acon.core.designsystem.component.dialog.AconPermissionDialog
 import com.acon.acon.core.designsystem.theme.AconTheme
+import com.acon.acon.core.utils.feature.action.BackOnPressed
 
 @Composable
 internal fun AreaVerificationHomeScreen(
     state: AreaVerificationHomeUiState,
     route: String,
-    modifier: Modifier = Modifier,
-    onNavigateToBack: () -> Unit = {}
+    onNextButtonClick: () -> Unit,
+    onPermissionSettingClick: () -> Unit,
+    modifier: Modifier = Modifier
 ) {
+    val context = LocalContext.current
     val configuration = LocalConfiguration.current
     val screenHeightDp = configuration.screenHeightDp
     val offsetY = (screenHeightDp * 0.65f).dp
-    val offsetBackGroundY = (screenHeightDp * 0.2f).dp
+
+    if (state.showPermissionDialog) {
+        AconPermissionDialog(
+            onPermissionGranted = onPermissionSettingClick
+        )
+    }
+
+    if(route != stringResource(com.acon.acon.feature.areaverification.R.string.area_verification_settings)) {
+        BackOnPressed(context)
+    }
 
     when (state) {
-        AreaVerificationHomeUiState.Success -> {
+        is AreaVerificationHomeUiState -> {
             Box(
                 modifier = modifier
-                    .background(AconTheme.color.Gray900)
+                    .paint(
+                        painterResource(R.drawable.area_verification_home_bg),
+                        contentScale = ContentScale.FillBounds
+                    )
             ) {
-                AreaVerificationHomeBackGround(
-                    screenHeightDp = screenHeightDp,
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .offset {
-                            IntOffset(
-                                x = 0,
-                                y = offsetBackGroundY.roundToPx()
-                            )
-                        }
-                )
-
                 Column(
                     modifier = Modifier
-                        .fillMaxSize(),
+                        .fillMaxSize()
+                        .navigationBarsPadding(),
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
                     Spacer(modifier = Modifier.height(offsetY))
@@ -92,10 +89,10 @@ internal fun AreaVerificationHomeScreen(
                     )
 
                     AconFilledButton(
-                        onClick = {},
+                        onClick = { onNextButtonClick() },
                         modifier = Modifier
                             .fillMaxWidth()
-                            .padding(top = 16.dp, bottom = 36.dp)
+                            .padding(top = 16.dp, bottom = 20.dp)
                             .padding(horizontal = 16.dp)
                     ) {
                         Text(
@@ -111,83 +108,15 @@ internal fun AreaVerificationHomeScreen(
     }
 }
 
-@Composable
-fun AreaVerificationHomeBackGround(
-    screenHeightDp: Int,
-    modifier: Modifier = Modifier
-) {
-    val offsetY = (screenHeightDp * 0.33f).dp
-    val glowEffectColor = AconTheme.color.GlowOrange
-    val white = AconTheme.color.White
-
-    Box(
-        modifier = modifier
-    ) {
-        Image(
-            imageVector = ImageVector.vectorResource(R.drawable.area_verification_home_bg),
-            contentDescription = "배경",
-            contentScale = ContentScale.Fit,
-            modifier = Modifier
-                .matchParentSize()
-        )
-        Box(
-            modifier = Modifier
-                .matchParentSize()
-        ) {
-            Box(
-                modifier = Modifier
-                    .align(Alignment.TopCenter)
-                    .offset(y = offsetY)
-            ) {
-                Canvas(
-                    modifier = Modifier
-                        .size(36.dp)
-                        .align(Alignment.Center)
-                ) {
-                    drawCircle(
-                        brush = Brush.radialGradient(
-                            colors = listOf(
-                                glowEffectColor,
-                                Color.Transparent
-                            )
-                        ),
-                        radius = size.minDimension / 2f,
-                        center = center
-                    )
-                }
-                Canvas(
-                    modifier = Modifier
-                        .size(16.dp)
-                        .align(Alignment.Center)
-                        .innerShadow(
-                            shape = CircleShape,
-                            color = AconTheme.color.GlowInnerShadow,
-                            blur = 20.dp,
-                            offsetY = 0.dp,
-                            offsetX = 4.dp,
-                            spread = 0.dp
-                        )
-                ) {
-                    drawCircle(
-                        color = white,
-                        radius = size.minDimension / 2f,
-                        style = Stroke(
-                            width = 2.dp.toPx()
-                        )
-                    )
-                }
-            }
-        }
-    }
-}
-
 @Preview
 @Composable
 private fun AreaVerificationHomeScreenPreview() {
     AconTheme {
         AreaVerificationHomeScreen(
-            state = AreaVerificationHomeUiState.Success,
-            route = ""
+            state = AreaVerificationHomeUiState(),
+            route = "",
+            onPermissionSettingClick = {},
+            onNextButtonClick = {}
         )
     }
 }
