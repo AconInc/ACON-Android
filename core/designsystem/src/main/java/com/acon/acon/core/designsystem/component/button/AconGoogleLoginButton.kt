e06b4451978edb708eeac491078fb864c06c1a42
seongmin.kim <arcn1345@gmail.com>
2025-05-28 23:30:21 +0900

    mod: 구글 로그인 버튼 컴포넌트 수정

--- core/designsystem/src/main/java/com/acon/acon/core/designsystem/component/button/AconGoogleLoginButton.kt
+++ core/designsystem/src/main/java/com/acon/acon/core/designsystem/component/button/AconGoogleLoginButton.kt
@@ -1,78 +1,74 @@
 package com.acon.acon.core.designsystem.component.button
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.acon.acon.core.designsystem.R
+import com.acon.acon.core.designsystem.component.button.v2.AconFilledButton
 import com.acon.acon.core.designsystem.theme.AconTheme
+
 @Composable
 fun AconGoogleLoginButton(
     onClick: () -> Unit = {},
-    modifier: Modifier = Modifier,
-    backgroundColor: Color = AconTheme.color.White,
-    borderColor: Color = AconTheme.color.Gray1,
-    borderWidth: Dp = 1.dp,
-    cornerRadius: Dp = 6.dp,
-    textColor: Color = AconTheme.color.Black,
-    textStyle: TextStyle = AconTheme.typography.subtitle2_14_med,
+    modifier: Modifier = Modifier
 ) {
-    AconButton(
-        backGroundColor = backgroundColor,
-        borderColor = borderColor,
-        modifier = modifier
-            .fillMaxWidth(),
-        borderWidth = borderWidth,
-        cornerRadius = cornerRadius,
-        contentPadding = PaddingValues(vertical = 12.dp),
+    AconFilledButton(
+        shape = RoundedCornerShape(percent = 50),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = AconTheme.color.White,
+            contentColor = AconTheme.color.Gray500
+        ),
+        modifier = modifier.fillMaxWidth(),
         onClick = onClick
     ) {
-        Row(
-           verticalAlignment = Alignment.CenterVertically
+        Box(
+            modifier = Modifier.fillMaxWidth()
         ) {
             Image(
                 imageVector = ImageVector.vectorResource(R.drawable.ic_google),
-                contentDescription = stringResource(R.string.google_login_btn_description)
+                contentDescription = stringResource(R.string.google_login_btn_description),
+                modifier = Modifier
+                    .align(Alignment.CenterStart)
+                    .padding(start = 24.dp)
             )
-            Spacer(modifier = Modifier.size(4.dp))
+
             Text(
                 text = stringResource(R.string.google_login_btn_content),
-                style = textStyle,
-                color = textColor,
-                textAlign = TextAlign.Center,
+                style = AconTheme.typography.Title4,
+                fontWeight = FontWeight.SemiBold,
                 maxLines = 1,
                 modifier = Modifier
-                    .align(Alignment.CenterVertically)
+                    .align(Alignment.Center)
             )
         }
     }
-
 }
 
 @Preview
 @Composable
 private fun PreviewAconGoogleLoginButton() {
     AconTheme {
-        AconGoogleLoginButton(
-            modifier = Modifier,
-            textColor = AconTheme.color.Black,
-            backgroundColor = AconTheme.color.White,
-            onClick = {},
-        )
+        Box(
+            modifier = Modifier.background(AconTheme.color.Black)
+        ) {
+            AconGoogleLoginButton(
+                modifier = Modifier,
+                onClick = {},
+            )
+        }
     }
 }
