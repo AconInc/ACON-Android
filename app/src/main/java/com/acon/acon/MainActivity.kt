0ea3f5ed2eb50f65786dde5cbb764702efb4bffd
Thirfir <sangbbang31@gmail.com>
2025-05-31 19:24:48 +0900

    feat: GPS 검사 로직 추가

--- app/src/main/java/com/acon/acon/MainActivity.kt
+++ app/src/main/java/com/acon/acon/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.acon.acon
 
+import android.content.IntentSender
 import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.IntentSenderRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.ui.Modifier
@@ -15,6 +19,12 @@ import com.acon.acon.core.designsystem.theme.AconTheme
 import com.acon.acon.domain.repository.SocialRepository
 import com.acon.acon.domain.repository.UserRepository
 import com.acon.acon.navigation.AconNavigation
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.LocationSettingsRequest
+import com.google.android.gms.location.Priority
+import com.google.android.gms.location.SettingsClient
 import dagger.hilt.android.AndroidEntryPoint
 import timber.log.Timber
 import javax.inject.Inject
@@ -26,6 +36,16 @@ class MainActivity : ComponentActivity() {
 
     @Inject
     lateinit var userRepository: UserRepository
+
+    private val gpsResolutionResultLauncher = registerForActivityResult(
+        ActivityResultContracts.StartIntentSenderForResult()
+    ) { result ->
+        if (result.resultCode == RESULT_OK) {
+            // User activated GPS
+        } else {
+            Toast.makeText(this, "GPS를 켜주세요.", Toast.LENGTH_SHORT).show()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
@@ -41,6 +61,8 @@ class MainActivity : ComponentActivity() {
         val windowInsetsController = WindowCompat.getInsetsController(window, window.decorView)
         windowInsetsController.isAppearanceLightStatusBars = false
 
+        checkGPS()
+
         setContent {
             AconTheme {
                 AconNavigation(
@@ -48,6 +70,30 @@ class MainActivity : ComponentActivity() {
                     navController = rememberNavController(),
                     userRepository = userRepository
                 )
+            }
+        }
+    }
+
+    private fun checkGPS() {
+        val locationRequest = LocationRequest.Builder(3_000L).setPriority(
+            Priority.PRIORITY_HIGH_ACCURACY
+        ).build()
+
+        val builder = LocationSettingsRequest.Builder()
+            .addLocationRequest(locationRequest)
+
+        val client: SettingsClient = LocationServices.getSettingsClient(this)
+        val task = client.checkLocationSettings(builder.build())
+        task.addOnSuccessListener {
+            // GPS 원래 활성화 상태
+        }.addOnFailureListener { exception ->
+            if (exception is ResolvableApiException) {
+                try {
+                    val intentSenderRequest = IntentSenderRequest.Builder(exception.resolution).build()
+                    gpsResolutionResultLauncher.launch(intentSenderRequest)
+                } catch (e: IntentSender.SendIntentException) {
+
+                }
             }
         }
     }
