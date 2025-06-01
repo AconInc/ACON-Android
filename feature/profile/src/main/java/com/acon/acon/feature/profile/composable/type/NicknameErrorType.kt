5c98d7a87541b8d4758345d49c4e3724442df897
seongmin.kim <arcn1345@gmail.com>
2025-06-01 22:08:39 +0900

    mod: 닉네임 텍스트필드 에러 처리 조건 수정 (최종)

--- feature/profile/src/main/java/com/acon/acon/feature/profile/composable/type/NicknameErrorType.kt
+++ feature/profile/src/main/java/com/acon/acon/feature/profile/composable/type/NicknameErrorType.kt
@@ -3,19 +3,16 @@ package com.acon.acon.feature.profile.composable.type
 import com.acon.acon.core.designsystem.R
 
 sealed class NicknameErrorType {
-    data object InvalidChar : NicknameErrorType()
-    data object InvalidLang : NicknameErrorType()
-    data object AlreadyUsed : NicknameErrorType()
+    data object Invalid : NicknameErrorType()
+    data object Duplicate : NicknameErrorType()
 }
 
 fun NicknameErrorType.validMessageResId(): Int = when (this) {
-    NicknameErrorType.InvalidChar -> R.string.nickname_error_invalid
-    NicknameErrorType.InvalidLang -> R.string.nickname_error_invalid_language
-    NicknameErrorType.AlreadyUsed -> R.string.nickname_error_duplicate
+    NicknameErrorType.Invalid -> R.string.nickname_error_invalid
+    NicknameErrorType.Duplicate -> R.string.nickname_error_duplicate
 }
 
 fun NicknameErrorType.contentDescriptionResId(): Int = when (this) {
-    NicknameErrorType.InvalidChar-> R.string.content_description_empty_nickname
-    NicknameErrorType.InvalidLang -> R.string.content_description_invalid_language
-    NicknameErrorType.AlreadyUsed -> R.string.content_description_duplicate_nickname
+    NicknameErrorType.Invalid-> R.string.content_description_empty_nickname
+    NicknameErrorType.Duplicate -> R.string.content_description_duplicate_nickname
 }
