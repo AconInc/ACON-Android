0d1e81c5d4306a7bf8dfd84c76712a45f58d30c6
seongmin.kim <arcn1345@gmail.com>
2025-06-03 02:03:29 +0900

    feat: 설정 strings 최종 적용 및 기존 파일 삭제

--- feature/settings/src/main/java/com/acon/acon/feature/withdraw/component/DeleteAccountTextField.kt
+++ feature/settings/src/main/java/com/acon/acon/feature/withdraw/component/DeleteAccountTextField.kt
@@ -25,8 +25,8 @@ import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.acon.acon.core.designsystem.R
 import com.acon.acon.core.designsystem.theme.AconTheme
-import com.acon.acon.feature.settings.R
 
 @Composable
 fun DeleteAccountTextField(
