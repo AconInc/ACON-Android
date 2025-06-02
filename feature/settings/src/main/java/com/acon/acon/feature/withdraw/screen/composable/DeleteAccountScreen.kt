185d4147e07a612154ede9a43e01840c64d582c2
seongmin.kim <arcn1345@gmail.com>
2025-06-03 01:26:59 +0900

    mod: 회원탈퇴 화면 탑바 글래스모피즘, gradient 적용

--- feature/settings/src/main/java/com/acon/acon/feature/withdraw/screen/composable/DeleteAccountScreen.kt
+++ feature/settings/src/main/java/com/acon/acon/feature/withdraw/screen/composable/DeleteAccountScreen.kt
@@ -3,6 +3,7 @@ package com.acon.acon.feature.withdraw.screen.composable
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -13,6 +14,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.relocation.BringIntoViewRequester
 import androidx.compose.foundation.relocation.bringIntoViewRequester
 import androidx.compose.foundation.rememberScrollState
@@ -42,11 +44,14 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 import com.acon.acon.core.designsystem.R
 import com.acon.acon.core.designsystem.component.button.v2.AconFilledButton
 import com.acon.acon.core.designsystem.component.radiobutton.AconRadioButton
 import com.acon.acon.core.designsystem.component.topbar.AconTopBar
 import com.acon.acon.core.designsystem.effect.LocalHazeState
+import com.acon.acon.core.designsystem.effect.defaultHazeEffect
+import com.acon.acon.core.designsystem.effect.imageGradientLayer
 import com.acon.acon.core.designsystem.keyboard.keyboardAsState
 import com.acon.acon.core.designsystem.theme.AconTheme
 import com.acon.acon.feature.withdraw.amplitude.deleteAccountAmplitudeExitReason
@@ -96,150 +101,163 @@ fun DeleteAccountScreen(
                 )
             }
 
-            Column(
-                modifier = modifier
-                    .fillMaxSize()
+            Box(
+                modifier = Modifier
                     .background(AconTheme.color.Gray900)
-                    .padding(horizontal = 16.dp)
-                    .navigationBarsPadding()
-                    .verticalScroll(scrollState)
-                    .hazeSource(LocalHazeState.current)
-                    .pointerInput(Unit) {
-                        detectTapGestures(
-                            onTap = {
-                                focusManager.clearFocus()
-                            }
-                        )
-                    }
+                    .statusBarsPadding()
+                    .fillMaxSize()
             ) {
-                Spacer(Modifier.height(42.dp))
-                Column {
-                    AconTopBar(
-                        modifier = Modifier.padding(vertical = 14.dp),
-                        paddingValues = PaddingValues(0.dp),
-                        leadingIcon = {
-                            IconButton(onClick = navigateBack) {
-                                Icon(
-                                    imageVector = ImageVector.vectorResource(R.drawable.ic_topbar_arrow_left),
-                                    contentDescription = stringResource(R.string.back),
-                                    tint = AconTheme.color.White
-                                )
-                            }
-                        },
-                        content = {
-                            Text(
-                                text = stringResource(R.string.settings_delete_account_topbar),
-                                style = AconTheme.typography.Title4,
-                                fontWeight = FontWeight.SemiBold,
-                                color = AconTheme.color.White
+                AconTopBar(
+                    paddingValues = PaddingValues(0.dp),
+                    leadingIcon = {
+                        IconButton(onClick = navigateBack) {
+                            Icon(
+                                imageVector = ImageVector.vectorResource(R.drawable.ic_topbar_arrow_left),
+                                contentDescription = stringResource(R.string.back),
+                                tint = AconTheme.color.White
                             )
-                        }
-                    )
-
-                    Spacer(Modifier.height(40.dp))
-                    Text(
-                        text = stringResource(R.string.settings_delete_account_title),
-                        style = AconTheme.typography.Headline4,
-                        fontWeight = FontWeight.SemiBold,
-                        color = AconTheme.color.White
-                    )
-
-                    Spacer(Modifier.height(8.dp))
-                    Text(
-                        text = stringResource(R.string.settings_delete_account_content),
-                        style = AconTheme.typography.Body1,
-                        color = AconTheme.color.Gray500
-                    )
-
-                    Spacer(Modifier.height(40.dp))
-                    Column(
-                        modifier = Modifier
-                            .imePadding(),
-                        verticalArrangement = Arrangement.spacedBy(16.dp)
-                    ) {
-                        DeleteReasonType.entries.forEach { reasonType ->
-                            val reason = stringResource(reasonType.reason)
-                            Row(
-                                verticalAlignment = Alignment.CenterVertically,
-                            ) {
-                                AconRadioButton(
-                                    selected = selectedReason == reasonType,
-                                    onClick = {
-                                        selectedReason = reasonType
-                                        onUpdateReason(reason)
-                                    }
-                                )
-                                Text(
-                                    text = reason,
-                                    style = AconTheme.typography.Title5,
-                                    color = AconTheme.color.White,
-                                    modifier = Modifier.padding(
-                                        start = 8.dp
-                                    )
-                                )
-                            }
-                        }
-                        if (selectedReason == DeleteReasonType.OTHER) {
-                            DeleteAccountTextField(
-                                value = otherReasonText,
-                                onValueChange = { otherReasonText = it },
-                                placeholder = stringResource(R.string.reason_other_placeholder),
-                                focusRequester = focusRequester,
-                                keyboardOptions = KeyboardOptions(
-                                    imeAction = ImeAction.Done
-                                ),
-                                keyboardActions = KeyboardActions(
-                                    onDone = {
-                                        focusManager.clearFocus()
-                                        onUpdateReason(otherReasonText)
-                                    }
-                                ),
-                                modifier = Modifier
-                                    .bringIntoViewRequester(bringIntoViewRequester)
-                                    .onFocusChanged { state ->
-                                        isFocused = state.isFocused
-                                        if (state.isFocused) {
-                                            scope.launch {
-                                                delay(300)
-                                                scrollState.animateScrollTo(scrollState.maxValue)
-                                                if (keyboardHeight > 0) {
-                                                    bringIntoViewRequester.bringIntoView()
-                                                }
-                                            }
-                                        } else {
-                                            onUpdateReason(otherReasonText)
-                                        }
-                                    }
-                            )
-                        }
-                        LaunchedEffect(keyboardHeight) {
-                            if (keyboardHeight == 0) {
-                                focusManager.clearFocus()
-                                isFocused = false
-                            }
-                        }
-                    }
-                }
-
-                Spacer(Modifier.weight(1f))
-                AconFilledButton(
-                    onClick = {
-                        if (submitButtonEnabled) {
-                            onBottomSheetShowStateChange(true)
-                            deleteAccountAmplitudeSubmit()
-                            deleteAccountAmplitudeExitReason(state.reason)
                         }
                     },
+                    content = {
+                        Text(
+                            text = stringResource(R.string.settings_delete_account_topbar),
+                            style = AconTheme.typography.Title4,
+                            fontWeight = FontWeight.SemiBold,
+                            color = AconTheme.color.White
+                        )
+                    },
                     modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(bottom = 24.dp),
-                    enabled = submitButtonEnabled
+                        .imageGradientLayer()
+                        .defaultHazeEffect(
+                            hazeState = LocalHazeState.current,
+                            tintColor = AconTheme.color.Gray900,
+                            blurRadius = 20.dp,
+                        )
+                        .zIndex(1f)
+                )
+
+                Column(
+                    modifier = modifier
+                        .fillMaxSize()
+                        .background(AconTheme.color.Gray900)
+                        .padding(horizontal = 16.dp)
+                        .navigationBarsPadding()
+                        .verticalScroll(scrollState)
+                        .hazeSource(LocalHazeState.current)
+                        .pointerInput(Unit) {
+                            detectTapGestures(
+                                onTap = {
+                                    focusManager.clearFocus()
+                                }
+                            )
+                        }
                 ) {
-                    Text(
-                        text = stringResource(R.string.submit),
-                        style = AconTheme.typography.Title4,
-                        fontWeight = FontWeight.SemiBold
-                    )
+                    Column {
+                        Text(
+                            text = stringResource(R.string.settings_delete_account_title),
+                            style = AconTheme.typography.Headline4,
+                            fontWeight = FontWeight.SemiBold,
+                            color = AconTheme.color.White,
+                            modifier = Modifier.padding(top = 56.dp)
+                        )
+
+                        Spacer(Modifier.height(8.dp))
+                        Text(
+                            text = stringResource(R.string.settings_delete_account_content),
+                            style = AconTheme.typography.Body1,
+                            color = AconTheme.color.Gray500
+                        )
+
+                        Spacer(Modifier.height(40.dp))
+                        Column(
+                            modifier = Modifier
+                                .imePadding(),
+                            verticalArrangement = Arrangement.spacedBy(16.dp)
+                        ) {
+                            DeleteReasonType.entries.forEach { reasonType ->
+                                val reason = stringResource(reasonType.reason)
+                                Row(
+                                    verticalAlignment = Alignment.CenterVertically,
+                                ) {
+                                    AconRadioButton(
+                                        selected = selectedReason == reasonType,
+                                        onClick = {
+                                            selectedReason = reasonType
+                                            onUpdateReason(reason)
+                                        }
+                                    )
+                                    Text(
+                                        text = reason,
+                                        style = AconTheme.typography.Title5,
+                                        color = AconTheme.color.White,
+                                        modifier = Modifier.padding(
+                                            start = 8.dp
+                                        )
+                                    )
+                                }
+                            }
+                            if (selectedReason == DeleteReasonType.OTHER) {
+                                DeleteAccountTextField(
+                                    value = otherReasonText,
+                                    onValueChange = { otherReasonText = it },
+                                    placeholder = stringResource(R.string.reason_other_placeholder),
+                                    focusRequester = focusRequester,
+                                    keyboardOptions = KeyboardOptions(
+                                        imeAction = ImeAction.Done
+                                    ),
+                                    keyboardActions = KeyboardActions(
+                                        onDone = {
+                                            focusManager.clearFocus()
+                                            onUpdateReason(otherReasonText)
+                                        }
+                                    ),
+                                    modifier = Modifier
+                                        .bringIntoViewRequester(bringIntoViewRequester)
+                                        .onFocusChanged { state ->
+                                            isFocused = state.isFocused
+                                            if (state.isFocused) {
+                                                scope.launch {
+                                                    delay(300)
+                                                    scrollState.animateScrollTo(scrollState.maxValue)
+                                                    if (keyboardHeight > 0) {
+                                                        bringIntoViewRequester.bringIntoView()
+                                                    }
+                                                }
+                                            } else {
+                                                onUpdateReason(otherReasonText)
+                                            }
+                                        }
+                                )
+                            }
+                            LaunchedEffect(keyboardHeight) {
+                                if (keyboardHeight == 0) {
+                                    focusManager.clearFocus()
+                                    isFocused = false
+                                }
+                            }
+                        }
+                    }
+
+                    Spacer(Modifier.weight(1f))
+                    AconFilledButton(
+                        onClick = {
+                            if (submitButtonEnabled) {
+                                onBottomSheetShowStateChange(true)
+                                deleteAccountAmplitudeSubmit()
+                                deleteAccountAmplitudeExitReason(state.reason)
+                            }
+                        },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(bottom = 24.dp),
+                        enabled = submitButtonEnabled
+                    ) {
+                        Text(
+                            text = stringResource(R.string.submit),
+                            style = AconTheme.typography.Title4,
+                            fontWeight = FontWeight.SemiBold
+                        )
+                    }
                 }
             }
         }
