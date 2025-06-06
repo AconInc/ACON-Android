db64a685b073fa69358646800ff14c7d5aad3cb4
seongmin.kim <arcn1345@gmail.com>
2025-06-06 20:03:42 +0900

    feat: 구글 애드몹 의존성 사용 기본 세팅

--- app/src/main/java/com/acon/acon/AconApplication.kt
+++ app/src/main/java/com/acon/acon/AconApplication.kt
@@ -3,6 +3,7 @@ package com.acon.acon
 import android.app.Application
 import com.acon.acon.core.utils.feature.amplitude.AconAmplitude
 import com.acon.acon.core.utils.feature.amplitude.AconTestAmplitude
+import com.google.android.gms.ads.MobileAds
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
@@ -10,6 +11,7 @@ class AconApplication: Application() {
 
     override fun onCreate() {
         super.onCreate()
+        MobileAds.initialize(this)
         AconAmplitude.initialize(this)
         AconTestAmplitude.initialize(this)
     }
--- app/src/main/java/com/acon/acon/MainActivity.kt
+++ app/src/main/java/com/acon/acon/MainActivity.kt
@@ -19,6 +19,8 @@ import com.acon.acon.core.designsystem.theme.AconTheme
 import com.acon.acon.domain.repository.SocialRepository
 import com.acon.acon.domain.repository.UserRepository
 import com.acon.acon.navigation.AconNavigation
+import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.RequestConfiguration
 import com.google.android.gms.common.api.ResolvableApiException
 import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationServices
@@ -52,6 +54,11 @@ class MainActivity : ComponentActivity() {
             installSplashScreen()
         }
         super.onCreate(savedInstanceState)
+
+        // TODO - 현재 기기를 테스트 디바이스로 등록 -> 테스트 광고 노출
+        val testDevices = listOf("559854319F9393CE36A962FE0E09E02B", "2470BB3FFC6EB2D0A79866A27F78FBCD")
+        val configuration = RequestConfiguration.Builder().setTestDeviceIds(testDevices).build()
+        MobileAds.setRequestConfiguration(configuration)
 
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
