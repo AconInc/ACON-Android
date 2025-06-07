ed038f0e0482e3a8012ed5efdfefa80e21fda8b6
seongmin.kim <arcn1345@gmail.com>
2025-06-07 21:10:16 +0900

    mod: SignInViewModel 자동로그인 로직 수정

--- domain/src/main/java/com/acon/acon/domain/repository/UserRepository.kt
+++ domain/src/main/java/com/acon/acon/domain/repository/UserRepository.kt
@@ -1,6 +1,7 @@
 package com.acon.acon.domain.repository
 
 import com.acon.acon.domain.model.user.VerificationStatus
+import com.acon.acon.domain.type.LocalVerificationType
 import com.acon.acon.domain.type.SocialType
 import com.acon.acon.domain.type.UserType
 import kotlinx.coroutines.flow.Flow
@@ -10,4 +11,7 @@ interface UserRepository {
     suspend fun logout(): Result<Unit>
     suspend fun deleteAccount(reason: String): Result<Unit>
     fun getUserType(): Flow<UserType>
+
+    fun getLocalVerificationType(): Flow<LocalVerificationType>
+    suspend fun updateLocalVerificationType(isVerified: Boolean)
 }
--- feature/signin/src/main/java/com/acon/acon/feature/signin/screen/SignInViewModel.kt
+++ feature/signin/src/main/java/com/acon/acon/feature/signin/screen/SignInViewModel.kt
@@ -2,32 +2,65 @@ package com.acon.acon.feature.signin.screen
 
 import com.acon.acon.core.utils.feature.base.BaseContainerHost
 import com.acon.acon.domain.error.user.CredentialException
-import com.acon.acon.domain.repository.UserRepository
 import com.acon.acon.domain.repository.SocialRepository
-import com.acon.acon.domain.repository.TokenRepository
+import com.acon.acon.domain.repository.UserRepository
+import com.acon.acon.domain.type.LocalVerificationType
+import com.acon.acon.domain.type.UserType
 import dagger.hilt.android.lifecycle.HiltViewModel
 import org.orbitmvi.orbit.Container
 import org.orbitmvi.orbit.viewmodel.container
+import timber.log.Timber
 import javax.inject.Inject
 import kotlin.coroutines.cancellation.CancellationException
 
 @HiltViewModel
 class SignInViewModel @Inject constructor(
-    private val tokenRepository: TokenRepository,
     private val userRepository: UserRepository
 ) : BaseContainerHost<SignInUiState, SignInSideEffect>() {
 
     override val container: Container<SignInUiState, SignInSideEffect> =
         container(initialState = SignInUiState.Loading) {
-            isTokenValid()
+            autoSignIn()
         }
+
+    private fun autoSignIn() = intent {
+        userRepository.getUserType().collect {
+            when (it) {
+                UserType.GUEST -> {
+                    Timber.tag(TAG).d("GUEST")
+                }
+
+                else -> {
+                    Timber.tag(TAG).d("USER")
+                    isAreaVerified()
+                }
+            }
+        }
+    }
+
+    private fun isAreaVerified() = intent {
+        userRepository.getLocalVerificationType().collect { verificationType ->
+            when (verificationType) {
+                LocalVerificationType.VERIFIED -> {
+                    Timber.tag(TAG).d("LocalVerificationType.VERIFIED")
+                    postSideEffect(SignInSideEffect.NavigateToSpotListView)
+                }
+                LocalVerificationType.NOT_VERIFIED -> {
+                    Timber.tag(TAG).d("LocalVerificationType.NOT_VERIFIED")
+                    postSideEffect(SignInSideEffect.NavigateToAreaVerification)
+                }
+            }
+        }
+    }
 
     fun googleLogin(socialRepository: SocialRepository) = intent {
         socialRepository.googleLogin()
             .onSuccess {
-                if(it.hasVerifiedArea) {
+                if (it.hasVerifiedArea) {
+                    userRepository.updateLocalVerificationType(true)
                     postSideEffect(SignInSideEffect.NavigateToSpotListView)
                 } else {
+                    userRepository.updateLocalVerificationType(false)
                     postSideEffect(SignInSideEffect.NavigateToAreaVerification)
                 }
             }
@@ -57,28 +90,6 @@ class SignInViewModel @Inject constructor(
             }
     }
 
-    private fun isTokenValid() = intent {
-        tokenRepository.getAccessToken().onSuccess { accessToken ->
-            if (!accessToken.isNullOrEmpty()) {
-                val areaVerificationResult = tokenRepository.getAreaVerification()
-
-                areaVerificationResult.fold(
-                    onSuccess = { isVerified ->
-                        if (isVerified) {
-                            postSideEffect(SignInSideEffect.NavigateToSpotListView)
-                        } else {
-                            postSideEffect(SignInSideEffect.NavigateToAreaVerification)
-                        }
-                    },
-                    onFailure = {
-
-                    }
-                )
-            }
-        }
-    }
-
-
     fun navigateToSpotListView() = intent {
         postSideEffect(
             SignInSideEffect.NavigateToSpotListView
@@ -97,18 +108,23 @@ class SignInViewModel @Inject constructor(
         )
     }
 
+    companion object {
+        const val TAG = "SignInViewModel"
+    }
 }
 
 sealed interface SignInUiState {
-    data object Success : SignInUiState
+    data class Success(
+        val localVerificationType: LocalVerificationType = LocalVerificationType.NOT_VERIFIED
+    ) : SignInUiState
     data object Loading : SignInUiState
-    data object LoadFailed: SignInUiState
+    data object LoadFailed : SignInUiState
 }
 
 sealed interface SignInSideEffect {
     data object ShowToastMessage : SignInSideEffect
     data object NavigateToSpotListView : SignInSideEffect
-    data object NavigateToAreaVerification: SignInSideEffect
+    data object NavigateToAreaVerification : SignInSideEffect
     data object OnClickTermsOfUse : SignInSideEffect
     data object OnClickPrivacyPolicy : SignInSideEffect
 }
