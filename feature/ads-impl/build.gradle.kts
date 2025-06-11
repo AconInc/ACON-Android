bc71ebb5a4c76418d49588af3a4564c0bee4f22a
Thirfir <sangbbang31@gmail.com>
2025-06-12 01:47:50 +0900

    chore: ads-impl build-logic 적용

--- feature/ads-impl/build.gradle.kts
+++ feature/ads-impl/build.gradle.kts
@@ -1,43 +1,16 @@
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.acon.android.library)
+    alias(libs.plugins.acon.android.library.compose)
+    alias(libs.plugins.acon.android.library.haze)
 }
 
 android {
     namespace = "com.acon.feature.ads_impl"
-    compileSdk = 35
-
-    defaultConfig {
-        minSdk = 28
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
 }
 
 dependencies {
+    implementation(projects.core.adsApi)
+    implementation(projects.core.designsystem)
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.google.services.ads)
 }
