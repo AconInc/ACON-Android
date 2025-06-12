873c2f4a98ca38c5a765f1535630b7f4814f0c2d
seongmin.kim <seongmin.kim@snowoncard.com>
2025-06-12 18:55:01 +0900

    feat: MediaPermissionBottomSheet 컴포넌트 추가

---
+++ feature/profile/src/main/java/com/acon/acon/feature/profile/composable/screen/MediaPermissionBottomSheet.kt
@@ -0,0 +1,76 @@
+package com.acon.acon.feature.profile.composable.screen
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.acon.acon.core.designsystem.component.bottomsheet.AconBottomSheet
+import com.acon.acon.core.designsystem.noRippleClickable
+import com.acon.acon.core.designsystem.theme.AconTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+internal fun MediaPermissionBottomSheet(
+    onDismiss: () -> Unit,
+    onClickPermissionCheck: () -> Unit,
+    onClickPermissionSettings: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    AconBottomSheet(
+        onDismissRequest = { onDismiss() }
+    ) {
+        Column(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(bottom = 115.dp)
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .noRippleClickable {
+                        onClickPermissionCheck()
+                    },
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Start
+            ) {
+                Text(
+                    text = "더 많은 사진선택", //stringResource(R.string.), // TODO - 문구 정해지면 수정
+                    color = AconTheme.color.White,
+                    style = AconTheme.typography.Title4,
+                    fontWeight = FontWeight.Normal,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp, vertical = 17.dp)
+                )
+            }
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .noRippleClickable {
+                        onClickPermissionSettings()
+                    },
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Start
+            ) {
+                Text(
+                    text = "모든 사진에 대한 접근 허용", //stringResource(R.string.), // TODO - 문구 정해지면 수정
+                    color = AconTheme.color.White,
+                    style = AconTheme.typography.Title4,
+                    fontWeight = FontWeight.Normal,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp, vertical = 17.dp)
+                )
+            }
+        }
+    }
+}
