f9996e04612bdfd9281d9adcf929dce128cb1df1
seongmin.kim <arcn1345@gmail.com>
2025-06-15 16:35:45 +0900

    fix: SpotList 뷰모델 원상복귀

--- feature/spot/src/main/java/com/acon/acon/feature/spot/screen/spotlist/SpotListViewModel.kt
+++ feature/spot/src/main/java/com/acon/acon/feature/spot/screen/spotlist/SpotListViewModel.kt
@@ -18,7 +18,6 @@ import com.acon.acon.domain.usecase.IsDistanceExceededUseCase
 import com.acon.acon.feature.spot.mock.spotListUiStateCafeMock
 import com.acon.acon.feature.spot.mock.spotListUiStateRestaurantMock
 import com.acon.feature.common.base.BaseContainerHost
-import com.acon.feature.common.location.isInKorea
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dagger.hilt.android.qualifiers.ApplicationContext
 import org.orbitmvi.orbit.annotation.OrbitExperimental
@@ -60,22 +59,24 @@ class SpotListViewModel @Inject constructor(
 
             runOn<SpotListUiStateV2.Loading> {
                 initialLocation = location
-
-                if (location.isInKorea(context)) {
-                    fetchSpotList(location, Condition(SpotType.RESTAURANT, emptyList())) {
-                        SpotListUiStateV2.Success(
-                            transportMode = it.transportMode,
-                            spotList = it.spots,
-                            headTitle = "최고의 선택.",
-                            selectedSpotType = SpotType.RESTAURANT,
-                            currentLocation = location
-                        )
-                    }
-                } else {
-                    reduce {
-                        SpotListUiStateV2.OutOfServiceArea(state.selectedSpotType)
-                    }
+                reduce {
+                    spotListUiStateRestaurantMock
                 }
+//                if (location.isInKorea(context)) {
+//                    fetchSpotList(location, Condition(SpotType.RESTAURANT, emptyList())) {
+//                        SpotListUiStateV2.Success(
+//                            transportMode = it.transportMode,
+//                            spotList = it.spots,
+//                            headTitle = "최고의 선택.",
+//                            selectedSpotType = SpotType.RESTAURANT,
+//                            currentLocation = location
+//                        )
+//                    }
+//                } else {
+//                    reduce {
+//                        SpotListUiStateV2.OutOfServiceArea(state.selectedSpotType)
+//                    }
+//                }
             }
         }
     }
@@ -87,20 +88,27 @@ class SpotListViewModel @Inject constructor(
                 state.copy(selectedSpotType = spotType)
             }
 
-            fetchSpotList(
-                location = state.currentLocation,
-                condition = Condition(spotType, emptyList())
-            ) {
-                SpotListUiStateV2.Success(
-                    transportMode = it.transportMode,
-                    spotList = it.spots,
-                    headTitle = "최고의 선택.",
-                    selectedSpotType = spotType,
-                    currentLocation = state.currentLocation,
-                    selectedRestaurantFilters = emptyMap(),
-                    selectedCafeFilters = emptyMap()
-                )
+            reduce {
+                if (spotType == SpotType.RESTAURANT) {
+                    spotListUiStateRestaurantMock
+                } else {
+                    spotListUiStateCafeMock
+                }
             }
+//            fetchSpotList(
+//                location = state.currentLocation,
+//                condition = Condition(spotType, emptyList())
+//            ) {
+//                SpotListUiStateV2.Success(
+//                    transportMode = it.transportMode,
+//                    spotList = it.spots,
+//                    headTitle = "최고의 선택.",
+//                    selectedSpotType = spotType,
+//                    currentLocation = state.currentLocation,
+//                    selectedRestaurantFilters = emptyMap(),
+//                    selectedCafeFilters = emptyMap()
+//                )
+//            }
         }
     }
 
