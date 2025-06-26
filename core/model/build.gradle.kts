94b602ad2dba0e3249f76bba1e2e5b105cdbda3a
Thirfir <sangbbang31@gmail.com>
2025-06-27 02:32:34 +0900

    chore: core-model 모듈 생성 | domain, core-ui 모듈에 의존성 추가

---
+++ core/model/build.gradle.kts
@@ -0,0 +1,9 @@
+plugins {
+    alias(libs.plugins.acon.non.android.library)
+    alias(libs.plugins.kotlin.serialization)
+}
+
+dependencies {
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.compose.stable.marker)
+}
