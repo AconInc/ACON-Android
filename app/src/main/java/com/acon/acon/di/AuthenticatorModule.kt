364208cfff43c2c321b7785fe5c48350b19d3707
Thirfir <sangbbang31@gmail.com>
2025-07-04 01:33:13 +0900

    fix: Authenticator 모듈 빌드 수정

--- app/src/main/java/com/acon/acon/di/AuthenticatorModule.kt
+++ app/src/main/java/com/acon/acon/di/AuthenticatorModule.kt
@@ -1,10 +1,11 @@
 package com.acon.acon.di
 
 import android.content.Context
-import com.acon.acon.data.authentication.AuthAuthenticator
 import com.acon.acon.core.launcher.AppLauncher
-import com.acon.acon.data.session.SessionHandler
+import com.acon.acon.data.api.remote.UserApi
+import com.acon.acon.data.authentication.AuthAuthenticator
 import com.acon.acon.data.datasource.local.TokenLocalDataSource
+import com.acon.acon.data.session.SessionHandler
 import com.acon.acon.launcher.AppLauncherImpl
 import dagger.Binds
 import dagger.Module
@@ -25,9 +26,9 @@ object AuthenticatorModule {
         @ApplicationContext context: Context,
         tokenLocalDataSource: TokenLocalDataSource,
         sessionHandler: SessionHandler,
-        reissueTokenApi: ReissueTokenApi,
+        userApi: UserApi,
         appLauncher: AppLauncher
-    ): Authenticator = AuthAuthenticator(context, tokenLocalDataSource, sessionHandler, reissueTokenApi, appLauncher)
+    ): Authenticator = AuthAuthenticator(context, tokenLocalDataSource, sessionHandler, userApi, appLauncher)
 }
 
 @Module
