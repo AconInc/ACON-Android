718e42a831c38ecf1de2fd4c755b51cae28bce82
seongmin.kim <arcn1345@gmail.com>
2025-07-13 19:19:35 +0900

    mod: ProfileModScreen 수정

--- feature/profile/src/main/java/com/acon/acon/feature/profile/composable/screen/profileMod/composable/ProfileModScreenContainer.kt
+++ feature/profile/src/main/java/com/acon/acon/feature/profile/composable/screen/profileMod/composable/ProfileModScreenContainer.kt
@@ -1,13 +1,9 @@
 package com.acon.acon.feature.profile.composable.screen.profileMod.composable
 
-import android.content.Intent
-import android.net.Uri
-import android.provider.Settings
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.acon.acon.feature.profile.composable.screen.profileMod.ProfileModSideEffect
 import com.acon.acon.feature.profile.composable.screen.profileMod.ProfileModViewModel
@@ -21,10 +17,8 @@ fun ProfileModScreenContainer(
     selectedPhotoId: String? = null,
     onNavigateToBack: () -> Unit = {},
     onClickComplete: () -> Unit = {},
-    onNavigateToCustomGallery: () -> Unit = {}
 ) {
     val state by viewModel.collectAsState()
-    val context = LocalContext.current
 
     LaunchedEffect(selectedPhotoId) {
         if (selectedPhotoId?.isNotEmpty() == true) {
@@ -34,19 +28,8 @@ fun ProfileModScreenContainer(
 
     viewModel.collectSideEffect { effect ->
         when (effect) {
-            is ProfileModSideEffect.NavigateToSettings -> {
-                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
-                    data = Uri.fromParts("package", effect.packageName, null)
-                }
-                context.startActivity(intent)
-            }
-
             is ProfileModSideEffect.NavigateBack -> {
                 onNavigateToBack()
-            }
-
-            is ProfileModSideEffect.NavigateToCustomGallery -> {
-                onNavigateToCustomGallery()
             }
 
             is ProfileModSideEffect.UpdateProfileImage -> {
@@ -65,17 +48,11 @@ fun ProfileModScreenContainer(
         modifier = modifier,
         state = state,
         navigateToBack = viewModel::navigateToBack,
-        navigateToCustomGallery = viewModel::navigateToCustomGallery,
         onNicknameChanged = viewModel::onNicknameChanged,
         onBirthdayChanged = viewModel::onBirthdayChanged,
         onFocusChanged = viewModel::onFocusChanged,
         onRequestExitDialog = viewModel::onRequestExitDialog,
         onDisMissExitDialog = viewModel::onDisMissExitDialog,
-        onRequestPhotoPermission = viewModel::onRequestPhotoPermission,
-        onPhotoPermissionDenied = viewModel::onPhotoPermissionDenied,
-        onRequestPermissionDialog = viewModel::onRequestPermissionDialog,
-        onDisMissPermissionDialog = viewModel::onDisMissPermissionDialog,
-        moveToSettings = viewModel::onPermissionSettingClick,
         onRequestProfileEditModal = viewModel::onRequestProfileEditModal,
         onDisMissProfileEditModal = viewModel::onDisMissProfileEditModal,
         onUpdateProfileImage = viewModel::updateProfileImage,
