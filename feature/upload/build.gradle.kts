583467533dead3593f08c8d56f0dffffda0cc118
seongmin.kim <arcn1345@gmail.com>
2025-07-23 22:50:22 +0900

    add: 업로드 모듈 build.gradle coil 의존성 추가

--- feature/upload/build.gradle.kts
+++ feature/upload/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     alias(libs.plugins.acon.android.library.hilt)
     alias(libs.plugins.acon.android.library.orbit)
     alias(libs.plugins.acon.android.library.haze)
+    alias(libs.plugins.acon.android.library.coil)
 }
 
 android {
