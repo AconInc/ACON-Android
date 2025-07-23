2b6656c7f278c5aa67b4cb829d19dc33d58daab6
seongmin.kim <arcn1345@gmail.com>
2025-07-23 22:47:22 +0900

    feat: UploadPlaceSelectItem 컴포넌트 추가 (선택 아이템)

---
+++ feature/upload/src/main/java/com/acon/acon/feature/upload/screen/composable/add/UploadPlaceSelectItem.kt
@@ -0,0 +1,88 @@
+package com.acon.acon.feature.upload.screen.composable.add
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.acon.acon.core.designsystem.R
+import com.acon.acon.core.designsystem.theme.AconTheme
+
+@Composable
+internal fun UploadPlaceSelectItem(
+    title: String,
+    modifier: Modifier = Modifier,
+    shape: Shape = RoundedCornerShape(10.dp),
+    isSelected: Boolean = false
+) {
+    val backgroundColor = if(isSelected) {
+        AconTheme.color.GlassWhiteSelected
+    } else {
+        AconTheme.color.GlassWhiteDefault
+    }
+
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .clip(shape = shape)
+            .background(backgroundColor)
+            .then(
+                if (isSelected) {
+                    Modifier.border(
+                        width = 1.dp,
+                        color = backgroundColor,
+                        shape = shape
+                    )
+                } else {
+                    Modifier
+                }
+            )
+            .padding(vertical = 12.dp, horizontal = 14.dp)
+            .padding(start = 6.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = title,
+            style = AconTheme.typography.Title4,
+            color = AconTheme.color.White,
+            fontWeight = FontWeight.Normal
+        )
+
+        Spacer(Modifier.weight(1f))
+        Image(
+            imageVector = ImageVector.vectorResource(
+                if (isSelected) {
+                    R.drawable.ic_place_selected
+                } else {
+                    R.drawable.ic_place_un_selected
+                }
+            ),
+            contentDescription = stringResource(R.string.select)
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun UploadPlaceSelectItemPreView() {
+    AconTheme {
+        UploadPlaceSelectItem(
+            title = "카페"
+        )
+    }
+}
