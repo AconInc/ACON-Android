d45a1df2181c09391851fddba8ee38b6a3a5d541
seongmin.kim <arcn1345@gmail.com>
2025-07-28 22:33:06 +0900

    feat: 장소 선택 UI 로직 구현 및 뷰모델 연결

--- feature/upload/src/main/java/com/acon/acon/feature/upload/screen/composable/add/place/UploadSelectPlaceScreen.kt
+++ feature/upload/src/main/java/com/acon/acon/feature/upload/screen/composable/add/place/UploadSelectPlaceScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -17,12 +18,21 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.acon.acon.core.designsystem.R
 import com.acon.acon.core.designsystem.theme.AconTheme
+import com.acon.acon.core.model.type.SpotType
+import com.acon.acon.feature.upload.screen.UploadPlaceUiState
 import com.acon.acon.feature.upload.screen.composable.add.UploadPlaceSelectItem
+import com.acon.acon.feature.upload.screen.composable.type.getNameResId
 
 @Composable
 internal fun UploadSelectPlaceScreen(
-
+    state: UploadPlaceUiState,
+    onSelectSpotType: (SpotType) -> Unit,
+    onUpdateNextPageBtnEnabled: (Boolean) -> Unit
 ) {
+    LaunchedEffect(state.selectedSpotType) {
+        onUpdateNextPageBtnEnabled(state.selectedSpotType != null)
+    }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -59,11 +69,18 @@ internal fun UploadSelectPlaceScreen(
             verticalArrangement = Arrangement.spacedBy(12.dp)
         ) {
             UploadPlaceSelectItem(
-                title = stringResource(R.string.restaurant),
-                isSelected = true
+                title = stringResource(SpotType.RESTAURANT.getNameResId()),
+                isSelected = state.selectedSpotType == SpotType.RESTAURANT,
+                onClickUploadPlaceSelectItem = {
+                    onSelectSpotType(SpotType.RESTAURANT)
+                }
             )
             UploadPlaceSelectItem(
-                title = stringResource(R.string.cafe)
+                title = stringResource(SpotType.CAFE.getNameResId()),
+                isSelected = state.selectedSpotType == SpotType.CAFE,
+                onClickUploadPlaceSelectItem = {
+                    onSelectSpotType(SpotType.CAFE)
+                }
             )
         }
     }
@@ -73,6 +90,10 @@ internal fun UploadSelectPlaceScreen(
 @Composable
 private fun UploadSelectPlaceScreenPreview() {
     AconTheme {
-        UploadSelectPlaceScreen()
+        UploadSelectPlaceScreen(
+            state = UploadPlaceUiState(),
+            onSelectSpotType = {},
+            onUpdateNextPageBtnEnabled = {},
+        )
     }
 }
