2585162cfde3192e4b7fbe6bd2df7a698d780e55
Thirfir <sangbbang31@gmail.com>
2025-08-22 00:16:13 +0900

    feat: 온보딩 뷰모델 정의
    기본 UI 상호작용 및 상태 정의

---
+++ feature/onboarding/src/main/java/com/acon/feature/onboarding/introduce/viewmodel/IntroduceViewModel.kt
@@ -0,0 +1,50 @@
+package com.acon.feature.onboarding.introduce.viewmodel
+
+import androidx.compose.runtime.Immutable
+import com.acon.acon.core.ui.base.BaseContainerHost
+import com.acon.acon.domain.repository.OnboardingRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import org.orbitmvi.orbit.Container
+import org.orbitmvi.orbit.viewmodel.container
+import javax.inject.Inject
+
+@HiltViewModel
+class IntroduceViewModel @Inject constructor(
+    private val onboardingRepository: OnboardingRepository
+) : BaseContainerHost<IntroduceState, Unit>() {
+
+    override val container: Container<IntroduceState, Unit> = container(
+        initialState = IntroduceState()
+    )
+
+    fun onIntroduceTop50ScreenRendered() = intent {
+        reduce {
+            state.copy(
+                shouldShowLocalReviewScreenAnimation = false
+            )
+        }
+    }
+
+    fun onIntroduceMainScreenRendered() = intent {
+        reduce {
+            state.copy(
+                shouldShowTop50ScreenAnimation = false
+            )
+        }
+    }
+
+    fun onIntroduceMainScreenDisposed() = intent {
+        reduce {
+            state.copy(
+                shouldShowMainScreenAnimation = false
+            )
+        }
+    }
+}
+
+@Immutable
+data class IntroduceState(
+    val shouldShowLocalReviewScreenAnimation: Boolean = true,
+    val shouldShowTop50ScreenAnimation: Boolean = true,
+    val shouldShowMainScreenAnimation: Boolean = true
+)
