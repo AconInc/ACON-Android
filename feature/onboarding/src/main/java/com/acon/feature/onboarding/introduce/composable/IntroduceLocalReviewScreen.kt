0ba9d8c70187b1e20f3c59710cf50b369ff0e347
Thirfir <sangbbang31@gmail.com>
2025-08-21 20:42:16 +0900

    feat: 로컬 리뷰 온보딩 화면 태그 fading 애니메이션 적용

--- feature/onboarding/src/main/java/com/acon/feature/onboarding/introduce/composable/IntroduceLocalReviewScreen.kt
+++ feature/onboarding/src/main/java/com/acon/feature/onboarding/introduce/composable/IntroduceLocalReviewScreen.kt
@@ -1,25 +1,37 @@
 package com.acon.feature.onboarding.introduce.composable
 
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.platform.LocalInspectionMode
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.acon.acon.core.designsystem.R
 import com.acon.acon.core.designsystem.component.tag.AconTag
+import com.acon.acon.core.designsystem.effect.effect.shadowLayerBackground
 import com.acon.acon.core.designsystem.effect.screenDefault
 import com.acon.acon.core.designsystem.theme.AconTheme
 import com.acon.acon.core.ui.base.ScreenProvider
@@ -32,15 +44,27 @@ internal class IntroduceLocalReviewScreenProvider : ScreenProvider {
     @Composable
     override fun provide() {
         IntroduceLocalReviewScreen(
-            modifier = Modifier.padding(top = 20.dp)
+            modifier = Modifier.navigationBarsPadding().padding(top = 54.dp)
         )
     }
 }
+
+private const val LOCAL_TAG_ALPHA_ANIMATION_DURATION_MS = 1000
 
 @Composable
 internal fun IntroduceLocalReviewScreen(
     modifier: Modifier = Modifier
 ) {
+    val infiniteTransition = rememberInfiniteTransition(label = "tagAlphaInfinite")
+    val localTagAlphaAnimation by infiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = 1f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(durationMillis = LOCAL_TAG_ALPHA_ANIMATION_DURATION_MS, easing = LinearEasing),
+            repeatMode = RepeatMode.Reverse
+        ),
+        label = "tagAlphaAnimation"
+    )
 
     Column(
         modifier = modifier,
@@ -57,7 +81,8 @@ internal fun IntroduceLocalReviewScreen(
             text = stringResource(R.string.introduce_local_review_content),
             color = AconTheme.color.White,
             style = AconTheme.typography.Title4,
-            modifier = Modifier.padding(top = 24.dp)
+            modifier = Modifier.padding(top = 24.dp),
+            textAlign = TextAlign.Center
         )
 
         Box(
@@ -91,7 +116,16 @@ internal fun IntroduceLocalReviewScreen(
         AconTag(
             text = stringResource(R.string.store_tag_local),
             backgroundColor = AconTheme.color.TagLocal,
-            modifier = Modifier.padding(top = 40.dp)
+            modifier = Modifier.padding(top = 40.dp).shadowLayerBackground(
+                shadowColor = AconTheme.color.TagLocal,
+                shadowAlpha = localTagAlphaAnimation,
+                shadowRadius = 100f
+            ).alpha(localTagAlphaAnimation),
+            textStyle = AconTheme.typography.Body1.copy(
+                color = AconTheme.color.White,
+                fontWeight = FontWeight.SemiBold
+            ),
+            contentPadding = PaddingValues(horizontal = 14.dp, vertical = 5.dp)
         )
 
         Spacer(modifier = Modifier.height(137.dp))
@@ -102,6 +136,6 @@ internal fun IntroduceLocalReviewScreen(
 @Preview
 private fun IntroduceReviewScreenPreview() {
     IntroduceLocalReviewScreen(
-        modifier = Modifier.screenDefault().padding(top = 20.dp)
+        modifier = Modifier.screenDefault().padding(top = 54.dp)
     )
 }
