be951e013d9e72ba02cdc30587aea08132176b4c
Thirfir <sangbbang31@gmail.com>
2025-08-21 21:58:43 +0900

    feat: 로컬 리뷰 온보딩 UI 애니메이션 완성

--- feature/onboarding/src/main/java/com/acon/feature/onboarding/introduce/composable/IntroduceLocalReviewScreen.kt
+++ feature/onboarding/src/main/java/com/acon/feature/onboarding/introduce/composable/IntroduceLocalReviewScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
@@ -17,9 +18,15 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
@@ -30,6 +37,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.acon.acon.core.designsystem.R
+import com.acon.acon.core.designsystem.animation.slidingFadeIn
 import com.acon.acon.core.designsystem.component.tag.AconTag
 import com.acon.acon.core.designsystem.effect.effect.shadowLayerBackground
 import com.acon.acon.core.designsystem.effect.screenDefault
@@ -37,26 +45,59 @@ import com.acon.acon.core.designsystem.theme.AconTheme
 import com.acon.acon.core.ui.base.ScreenProvider
 import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
 import com.airbnb.lottie.compose.rememberLottieComposition
+import kotlinx.coroutines.delay
 
 internal class IntroduceLocalReviewScreenProvider : ScreenProvider {
 
     @Composable
     override fun provide() {
         IntroduceLocalReviewScreen(
-            modifier = Modifier.navigationBarsPadding().padding(top = 54.dp)
+            modifier = Modifier
+                .navigationBarsPadding()
+                .padding(top = 54.dp)
         )
     }
 }
 
 private const val LOCAL_TAG_ALPHA_ANIMATION_DURATION_MS = 1000
+private const val MESSAGE_APPEAR_ANIMATION_DELAY_MS = 200
+private const val MESSAGE_APPEAR_ANIMATION_DURATION_MS = 500
 
 @Composable
 internal fun IntroduceLocalReviewScreen(
     modifier: Modifier = Modifier
 ) {
-    val infiniteTransition = rememberInfiniteTransition(label = "tagAlphaInfinite")
-    val localTagAlphaAnimation by infiniteTransition.animateFloat(
+
+    var playAcornLottieAnimation by remember {
+        mutableStateOf(false)
+    }
+    val acornLottieComposition by rememberLottieComposition(
+        spec = LottieCompositionSpec.RawRes(R.raw.acorn5)
+    )
+    val acornLottieProgress = animateLottieCompositionAsState(
+        composition = acornLottieComposition,
+        isPlaying = playAcornLottieAnimation,
+        iterations = 1,
+        speed = 1f
+    )
+
+    var isAcornLottieAnimationEnded by remember {
+        mutableStateOf(false)
+    }
+    LaunchedEffect(Unit) {
+        delay((MESSAGE_APPEAR_ANIMATION_DELAY_MS + MESSAGE_APPEAR_ANIMATION_DURATION_MS).toLong())
+        playAcornLottieAnimation = true
+
+        delay(100)
+        snapshotFlow { acornLottieProgress.isAtEnd }.collect { isEnded ->
+            if (isEnded)
+                isAcornLottieAnimationEnded = true
+        }
+    }
+
+    val localTagAlphaAnimation by rememberInfiniteTransition(label = "tagAlphaInfinite").animateFloat(
         initialValue = 0f,
         targetValue = 1f,
         animationSpec = infiniteRepeatable(
@@ -70,20 +111,30 @@ internal fun IntroduceLocalReviewScreen(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(
-            text = stringResource(R.string.introduce_local_review_title),
-            color = AconTheme.color.White,
-            style = AconTheme.typography.Title2,
-            fontWeight = FontWeight.ExtraBold
-        )
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .slidingFadeIn(
+                    durationMillis = MESSAGE_APPEAR_ANIMATION_DURATION_MS,
+                    delayMillis = MESSAGE_APPEAR_ANIMATION_DELAY_MS
+                ),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = stringResource(R.string.introduce_local_review_title),
+                color = AconTheme.color.White,
+                style = AconTheme.typography.Title2,
+                fontWeight = FontWeight.ExtraBold
+            )
 
-        Text(
-            text = stringResource(R.string.introduce_local_review_content),
-            color = AconTheme.color.White,
-            style = AconTheme.typography.Title4,
-            modifier = Modifier.padding(top = 24.dp),
-            textAlign = TextAlign.Center
-        )
+            Text(
+                text = stringResource(R.string.introduce_local_review_content),
+                color = AconTheme.color.White,
+                style = AconTheme.typography.Title4,
+                modifier = Modifier.padding(top = 24.dp),
+                textAlign = TextAlign.Center
+            )
+        }
 
         Box(
             modifier = Modifier
@@ -94,9 +145,8 @@ internal fun IntroduceLocalReviewScreen(
         ) {
             if (LocalInspectionMode.current.not())
                 LottieAnimation(
-                    composition = rememberLottieComposition(
-                        spec = LottieCompositionSpec.RawRes(R.raw.acorn5)
-                    ).value,
+                    composition = acornLottieComposition,
+                    progress = { acornLottieProgress.progress },
                     modifier = Modifier
                         .fillMaxSize()
                         .aspectRatio(1f)
@@ -113,20 +163,34 @@ internal fun IntroduceLocalReviewScreen(
             }
         }
 
-        AconTag(
-            text = stringResource(R.string.store_tag_local),
-            backgroundColor = AconTheme.color.TagLocal,
-            modifier = Modifier.padding(top = 40.dp).shadowLayerBackground(
-                shadowColor = AconTheme.color.TagLocal,
-                shadowAlpha = localTagAlphaAnimation,
-                shadowRadius = 100f
-            ).alpha(localTagAlphaAnimation),
-            textStyle = AconTheme.typography.Body1.copy(
-                color = AconTheme.color.White,
-                fontWeight = FontWeight.SemiBold
-            ),
-            contentPadding = PaddingValues(horizontal = 14.dp, vertical = 5.dp)
-        )
+        Box(
+            modifier = Modifier
+                .height(IntrinsicSize.Min)
+                .width(IntrinsicSize.Min)
+        ) {
+            AconTag(
+                text = stringResource(R.string.store_tag_local),
+                backgroundColor = AconTheme.color.TagLocal,
+                modifier = Modifier
+                    .padding(top = 40.dp)
+                    .then(
+                        if (isAcornLottieAnimationEnded)
+                            Modifier
+                                .shadowLayerBackground(
+                                    shadowColor = AconTheme.color.TagLocal,
+                                    shadowAlpha = localTagAlphaAnimation,
+                                    shadowRadius = 100f
+                                )
+                                .alpha(localTagAlphaAnimation)
+                        else Modifier.alpha(0f)
+                    ),
+                textStyle = AconTheme.typography.Body1.copy(
+                    color = AconTheme.color.White,
+                    fontWeight = FontWeight.SemiBold
+                ),
+                contentPadding = PaddingValues(horizontal = 14.dp, vertical = 5.dp)
+            )
+        }
 
         Spacer(modifier = Modifier.height(137.dp))
     }
@@ -136,6 +200,8 @@ internal fun IntroduceLocalReviewScreen(
 @Preview
 private fun IntroduceReviewScreenPreview() {
     IntroduceLocalReviewScreen(
-        modifier = Modifier.screenDefault().padding(top = 54.dp)
+        modifier = Modifier
+            .screenDefault()
+            .padding(top = 54.dp)
     )
 }
